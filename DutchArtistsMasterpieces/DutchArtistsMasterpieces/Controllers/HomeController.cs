c01fcaaedb490c83771f71e4ae8dc94e17068c72
Eduardo <eduardo.cxcorreio@gmail.com>
2018-01-20 12:25:37 +0100

    Added the Controller and simple Razor View.

---
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchArtistsMasterpieces.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DutchArtistsMasterpieces.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IArtistRepository _artistRepository;
+
+        public HomeController(IArtistRepository artistRepository)
+        {
+            _artistRepository = artistRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Artist overview";
+
+            var artists = _artistRepository.GetAllArtists().OrderBy(a => a.Name);
+
+            return View(artists);
+        }
+    }
+}
--- DutchArtistsMasterpieces/DutchArtistsMasterpieces/Startup.cs
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchArtistsMasterpieces.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,8 @@ namespace DutchArtistsMasterpieces
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IArtistRepository, MockArtistRepository>();
+
             services.AddMvc();
         }
 
