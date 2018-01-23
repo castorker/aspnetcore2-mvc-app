4bcb7f5fa20d08ca8fd8cbb918c6d27e7b2ac4db
Eduardo <eduardo.cxcorreio@gmail.com>
2018-01-23 19:19:17 +0100

    Added Data with Entity Framework Core.

---
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Migrations/20180123180431_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DutchArtistsMasterpieces.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Birth = table.Column<DateTime>(nullable: false),
+                    BirthYear = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: true),
+                    Death = table.Column<DateTime>(nullable: false),
+                    DeathYear = table.Column<int>(nullable: false),
+                    ImageThumbnailUrl = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    IsArtistOfTheMonth = table.Column<bool>(nullable: false),
+                    LongDescription = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ShortDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
---
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Models/ArtistRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchArtistsMasterpieces.Models
+{
+    public class ArtistRepository : IArtistRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ArtistRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Artist> GetAllArtists()
+        {
+            return _appDbContext.Artists;
+        }
+
+        public Artist GetArtistById(int id)
+        {
+            return _appDbContext.Artists.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
