58c87767c1eacb82e82261ac241ab28da3e586b7
Eduardo <eduardo.cxcorreio@gmail.com>
2018-01-26 01:09:57 +0100

    Added Authentication and Authorization using ASP.NET Identity.

--- DutchArtistsMasterpieces/DutchArtistsMasterpieces/Startup.cs
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Startup.cs
@@ -6,6 +6,7 @@ using DutchArtistsMasterpieces.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,8 @@ namespace DutchArtistsMasterpieces
         {
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
             services.AddTransient<IArtistRepository, ArtistRepository>();
             services.AddTransient<IArtworkRepository, ArtworkRepository>();
             services.AddTransient<IReviewRepository, ReviewRepository>();
@@ -42,6 +45,7 @@ namespace DutchArtistsMasterpieces
                 app.UseDeveloperExceptionPage();
                 app.UseStatusCodePages();
                 app.UseStaticFiles();
+                app.UseAuthentication();
                 //app.UseMvcWithDefaultRoute();
 
                 app.UseMvc(routes =>
